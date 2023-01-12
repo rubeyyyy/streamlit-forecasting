9745b4338417c4e83e7e49c068e82174bad68f3f
Rubi Khatri <rubeyyy07@gmail.com>
2023-01-12 11:38:12 +0545

    visualize

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from prophet import Prophet
 from prophet.diagnostics import performance_metrics
 from prophet.diagnostics import cross_validation
 from prophet.plot import plot_cross_validation_metric
+import base64
 
 st.title(' Automated Time Series Forecasting')
 
@@ -31,3 +32,27 @@ if df is not None:
     m=Prophet()
     m.fit(data)
 
+#visualize and forecast data
+
+if df is not None:
+    future= m.make_future_dataframe(periods= periods_input)
+
+    forecast= m.predict(future)
+    fcst= forecast[['ds','yhat','yhat_lower','yhat_upper']]
+    fcst_filtered= fcst[fcst['ds']> max_date]
+    st.write(fcst_filtered)
+
+    fig1=m.plot(forecast)
+    st.write(fig1)
+
+    fig2= m.plot_components(forecast)
+    st.write(fig2)
+
+#download the forecast datat
+
+# if df is not None:
+#         csv_exp=fcst_filtered.to_csv(index=False)
+#         #When no file name is given, pandas returns the csv as a string,nice.
+#         b64=base64.b64decode(csv_exp.encode()).decode()
+#         href=f'<a href="data:file/csv ;base64,{b64}">Download CSV File </a>(right- click and save as *&lt;forecast_name&gt;.csv*)'
+#         st.markdown(href,unsafe_allow_html=True)
